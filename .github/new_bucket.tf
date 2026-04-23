eb84828fd552d219479fe5a8d8290f92ac807e64
Laura Astola <laura.astola@accenture.com>
2026-04-23 14:36:44 +0200

    try out terraform

---
+++ .github/new_bucket.tf
@@ -0,0 +1,25 @@
+# Specify the provider and access details
+
+provider "aws" {
+  
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "udabucket" {
+  bucket = "cicd-terraform-demo-bucket20213"
+
+  tags = {
+    Name        = "CICD test bucket"
+    Environment = "Dev"
+  }
+}
+
+# Create an EC2 instance
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-098e39bafa7e7303d"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "CICD test instance"
+  }
+}
